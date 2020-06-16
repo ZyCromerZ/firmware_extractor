154480a7f9b063987f8d0ecadb73085af3aca56a
Dani Tri Suardi <neetroid97@gmail.com>
2020-06-16 09:40:03 +0700

    initial

---
+++ file.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+git clone --recurse-submodules https://github.com/erfanoabdi/Firmware_extractor.git
+cd Firmware_extractor
+wget https://hugeota.d.miui.com/20.6.15/miui_BEGONIA_20.6.15_c85e084b31_10.0.zip -o firmware_begonia.zip
+./extractor.sh firmware_begonia.zip
+cd out
+git init
+git add . && git commit -s -m "miui_BEGONIA_20.6.15_c85e084b31_10.0"
+git checkout -b miui_BEGONIA_20.6.15_c85e084b31_10.0
+git branch -D master
+git remote add origin https://$githubKey@github.com/ZyCromerZ/begonia_files.git
+git push --all origin -f
